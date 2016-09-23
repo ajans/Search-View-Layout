5e25f088ff2265b1e6856dbd7866d6d4ecac2f0e
Arne Jans <aj@solute.de>
2016-09-23 11:33:47 +0200

    intermediate bugfix for mExpandedHeight DO NOT MERGE

--- searchviewlayout/src/main/java/xyz/sahildave/widget/SearchViewLayout.java
+++ searchviewlayout/src/main/java/xyz/sahildave/widget/SearchViewLayout.java
@@ -16,7 +16,6 @@
 
 package xyz.sahildave.widget;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -133,9 +132,6 @@ public class SearchViewLayout extends FrameLayout {
     public SearchViewLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         ANIMATION_DURATION = context.getResources().getInteger(R.integer.animation_duration);
-        if (context instanceof Activity) {
-            mExpandedHeight = Utils.getSizeOfScreen((Activity) context).y;
-        }
     }
 
     @Override
@@ -257,6 +253,7 @@ public class SearchViewLayout extends FrameLayout {
     public void setExpandedContentFragment(AppCompatActivity activity, Fragment contentFragment) {
         mExpandedContentFragment = contentFragment;
         mFragmentManager = activity.getSupportFragmentManager();
+        mExpandedHeight = Utils.getSizeOfScreen(activity).y;
     }
 
     /***
